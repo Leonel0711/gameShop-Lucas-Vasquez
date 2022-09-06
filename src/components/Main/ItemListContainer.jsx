86679bf04a0ce9bac8c761c3a53f1fd96366d7ab
Lucas Vasquez <lucasleonelvasquez0711@gmail.com>
2022-09-05 23:12:32 -0300

    Creacion de ItemCount y contador de producto

--- src/components/Main/ItemListContainer.jsx
+++ src/components/Main/ItemListContainer.jsx
@@ -1,4 +1,12 @@
-function ItemListContainer(props) {
-    return (<h1>{props.text}</h1>)
+import ItemCount from "./ItemCount";
+function ItemListContainer() {
+    const onAdd = (count)=>{
+        alert(`Se añadio ${count} productos`);
+    }
+    return (<section className="container containerProducts ">
+        <ItemCount stock={5} initial={1} onAdd={onAdd}/>
+        <ItemCount stock={0} initial={1} onAdd={onAdd}/>
+    </section>
+    )
 }
 export default ItemListContainer;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,6 @@ import "./main.css"
 import ItemListContainer from "./ItemListContainer";
 function Main() {
     return (
-        <main><ItemListContainer text="Aca estara mi catalogo de videojuegos"/></main>)
+        <main><ItemListContainer /></main>)
 }
 export default Main;
