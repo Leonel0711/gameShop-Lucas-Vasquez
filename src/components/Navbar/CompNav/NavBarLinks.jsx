75a2babd6b6050e5d66084de0055d04528aa097f
Lucas Vasquez <lucasleonelvasquez0711@gmail.com>
2022-09-10 19:32:54 -0300

    subida de gh-page

--- src/components/Navbar/CompNav/NavBarLinks.jsx
+++ src/components/Navbar/CompNav/NavBarLinks.jsx
@@ -4,9 +4,9 @@ function NavBarLinks() {
     return (
         <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div className="offcanvas-body">
-                <CommonLinks title="Home"/>
-                <CommonLinks title="Iniciar Sesión"/>
-                <Dropwdown title="Juegos" subLinks={["Acción", "Aventura", "Deportes", "Estrategia", "Fantasia", "Simuladores"]}/>
+                <CommonLinks title="Home" />
+                <CommonLinks title="Iniciar Sesión" />
+                <Dropwdown title="Juegos" subLinks={["Teclado", "Mouse", "Auriculares"]} />
             </div>
         </div>
     )
